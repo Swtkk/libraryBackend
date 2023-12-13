ff0dc465bf1e2a27bffe90ed716c394d4a81687d
marci <marcin123523@wp.pl>
2023-12-13 19:18:11 +0100

    .

--- src/main/java/library/demo/service/LibraryRepository.java
+++ src/main/java/library/demo/service/LibraryRepository.java
@@ -4,7 +4,6 @@ import library.demo.model.Book;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface LibraryRepository extends MongoRepository<Book, ObjectId> {
     Book findByTitle(String title);
